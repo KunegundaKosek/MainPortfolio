c502703f588160753f28dc96b11c844dd403ab8b
Kunegunda Kosek <kosekkunegunda@wp.pl>
2022-03-14 13:35:29 +0100

    Czesc 1

---
+++ foto.js
@@ -0,0 +1,16 @@
+//funkcja przewijania zdjęć;
+
+let number = Math.floor(Math.random()*5)+1;
+
+			
+			function changeSlide()
+			{
+				number++; if (number>5) number=1;
+				
+				var plik = "<img src=\"img/slide" + number + ".jpg\" />";
+				
+				document.getElementById("slider").innerHTML = plik;
+				
+				setTimeout("changeSlide()", 5000);
+			
+			}
---
+++ script.js
@@ -0,0 +1,47 @@
+//rozwijanie się menu
+
+ /*$( document ).ready( function () {
+    $( '.dropdown' ).on( 'click', function ( e ) {
+        var $el = $( this );
+        var $parent = $( this ).offsetParent( ".dropdown-menu" );
+        if ( !$( this ).next().hasClass( 'show' ) ) {
+            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
+        }
+        var $subMenu = $( this ).next( ".dropdown-menu" );
+        $subMenu.toggleClass( 'show' );
+        
+        $( this ).parent( "li" ).toggleClass( 'show' );
+
+        $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
+            $( '.dropdown-menu .show' ).removeClass( "show" );
+        } );
+        
+         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
+            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
+        }
+
+        return false;
+    } );
+} ); */
+
+//zegar na stronie głównej 
+
+function time() {
+    var today = new Date();
+    
+    var hour = today.getHours();
+    if (hour<10) hour = "0"+hour;
+    
+    var minute = today.getMinutes();
+    if (minute<10) minute = "0"+minute;
+    
+    var second = today.getSeconds();
+    if (second<10) second = "0"+second;
+    
+    document.getElementById("clock").innerHTML = hour+":"+minute+":"+second;
+     
+     setTimeout("time()",1000);
+}
+
+
+
