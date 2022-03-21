feb4fdb7df1480825864ae31529a4e768bb2d4bd
Kunegunda Kosek <kosekkunegunda@wp.pl>
2022-03-21 11:47:51 +0100

    Część3

--- ikonki.js
+++ ikonki.js
@@ -23,5 +23,169 @@ certificates.addEventListener("mouseout",function(){
 })
 
 //GitHub
+const git = document.getElementById('git')
+const gitTxt = document.getElementById('gitTxt')
+git.addEventListener("mouseover",function(){
+    gitTxt.innerHTML = "GitHub";
+})
 
+git.addEventListener("mouseout",function(){
+    gitTxt.innerHTML = "";
+})
 //Linkedin
+const li = document.getElementById('li')
+const liTxt = document.getElementById('liTxt')
+li.addEventListener("mouseover",function(){
+    liTxt.innerHTML = "LinkedIn";
+})
+
+li.addEventListener("mouseout",function(){
+    liTxt.innerHTML = "";
+})
+//projekty
+const code = document.getElementById('code')
+const codeTxt = document.getElementById('codeTxt')
+code.addEventListener("mouseover",function(){
+    codeTxt.innerHTML = "Projekty";
+})
+
+code.addEventListener("mouseout",function(){
+    codeTxt.innerHTML = "";
+})
+
+//informacje
+
+const info = document.getElementById('info')
+const infoTxt = document.getElementById('infoTxt')
+info.addEventListener("mouseover",function(){
+    infoTxt.innerHTML = "Informacje";
+})
+
+info.addEventListener("mouseout",function(){
+    infoTxt.innerHTML = "";
+})
+//w budowie #1
+const build = document.getElementById('build')
+const buildTxt = document.getElementById('buildTxt')
+build.addEventListener("mouseover",function(){
+    buildTxt.innerHTML = "Strona w budowie";
+})
+
+build.addEventListener("mouseout",function(){
+   buildTxt.innerHTML = "";
+})
+
+//w budowie #2
+const build2 = document.getElementById('build2')
+const build2Txt = document.getElementById('build2Txt')
+build2.addEventListener("mouseover",function(){
+    build2Txt.innerHTML = "Strona w budowie";
+})
+
+build2.addEventListener("mouseout",function(){
+   build2Txt.innerHTML = "";
+})
+
+//kalkulator
+const calc = document.getElementById('calc')
+const calcTxt = document.getElementById('calcTxt')
+calc.addEventListener("mouseover",function(){
+    calcTxt.innerHTML = "Kalkulator";
+})
+
+calc.addEventListener("mouseout",function(){
+   calcTxt.innerHTML = "";
+})
+//kalendarz
+const calendar = document.getElementById('calendar')
+const calendarTxt = document.getElementById('calendarTxt')
+calendar.addEventListener("mouseover",function(){
+    calendarTxt.innerHTML = "Kalendarz";
+})
+
+calendar.addEventListener("mouseout",function(){
+   calendarTxt.innerHTML = "";
+})
+//pogoda
+const weather = document.getElementById('weather')
+const weatherTxt = document.getElementById('weatherTxt')
+weather.addEventListener("mouseover",function(){
+    weatherTxt.innerHTML = "Pogoda";
+})
+
+weather.addEventListener("mouseout",function(){
+   weatherTxt.innerHTML = "";
+})
+// zegar
+const wath = document.getElementById('wath')
+const wathTxt = document.getElementById('wathTxt')
+wath.addEventListener("mouseover",function(){
+    wathTxt.innerHTML = "Zegar";
+})
+
+wath.addEventListener("mouseout",function(){
+   wathTxt.innerHTML = "";
+})
+//game
+const gamer = document.getElementById('gamer')
+const gamerTxt = document.getElementById('gamerTxt')
+gamer.addEventListener("mouseover",function(){
+    gamerTxt.innerHTML = "Gry";
+})
+
+gamer.addEventListener("mouseout",function(){
+   gamerTxt.innerHTML = "";
+})
+//blog o kotach
+const cat = document.getElementById('cat')
+const gamcat = document.getElementById('catTxt')
+cat.addEventListener("mouseover",function(){
+    catTxt.innerHTML = "Strona o kotkach";
+})
+
+cat.addEventListener("mouseout",function(){
+   catTxt.innerHTML = "";
+})
+//zgadnij liczbę
+const findNumber = document.getElementById('findNumber')
+const findNumberTxt = document.getElementById('findNumberTxt')
+findNumber.addEventListener("mouseover",function(){
+    findNumberTxt.innerHTML = "Gra 'zgadnij liczbę'";
+})
+
+findNumber.addEventListener("mouseout",function(){
+   findNumberTxt.innerHTML = "";
+})
+
+//strona w budowie
+const build3 = document.getElementById('build3')
+const build3Txt = document.getElementById('build3Txt')
+build3.addEventListener("mouseover",function(){
+    build3Txt.innerHTML = "Strona w budowie";
+})
+
+build3.addEventListener("mouseout",function(){
+   build3Txt.innerHTML = "";
+})
+
+//mapa
+const mapp = document.getElementById('mapp')
+const mappTxt = document.getElementById('mappTxt')
+mapp.addEventListener("mouseover",function(){
+    mappTxt.innerHTML = "Sprawdź mapę";
+})
+
+mapp.addEventListener("mouseout",function(){
+   mappTxt.innerHTML = "";
+})
+//kontakt
+
+const phone = document.getElementById('phone')
+const phoneTxt = document.getElementById('phoneTxt')
+phone.addEventListener("mouseover",function(){
+    phoneTxt.innerHTML = "kontakt";
+})
+
+phone.addEventListener("mouseout",function(){
+   phoneTxt.innerHTML = "";
+})
